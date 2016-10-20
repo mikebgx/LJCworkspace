5b266edc9b14c9f5c9e6e9fdbc2e8b4d3eaf4cb4
Mike Burton <mikeb@mycosystems.co.uk>
2016-10-20 11:01:50 -0400

    Updated README to point folks at new repo: https://github.com/LondonJavaCommunity/JavaWorkshop.git

--- S00_Hello/src/step02/Hello.java
+++
@@ -1,9 +0,0 @@
-package step02;
-
-public class Hello {
-
-	public static void main(String[] args) {
-		System.out.println( "Hello" );
-	}
-
-}
--- step01/Simple.java
+++
@@ -1,8 +0,0 @@
-
-public class Simple {
-
-	public static void main(String[] args) {
-		System.out.println("Hello from simple Java app");
-	}
-}
-
